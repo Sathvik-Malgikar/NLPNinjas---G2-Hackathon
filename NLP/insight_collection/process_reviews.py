e3e399914fa9d8576ab895a2c2f7197eb6ed3c46
Sathvik-Malgikar <Sathvik.malgikar@gmail.com>
2024-04-11 00:00:18 +0530

    split insights JSON into seperate outputs

--- NLP/insight_collection/process_reviews.py
+++ NLP/insight_collection/process_reviews.py
@@ -1,18 +1,18 @@
 import pandas as pd
 import json
 
-from review_components.regionwise import extract_country_name,extract_star_rating
-from review_components.getloveandhateinfo import get_love_and_hate_comment_keywords
-from review_components.vote_counter import extract_vote_counts,getvotesinfo
-from review_components.average_secondary_metrics import extract_secondary_comments_value,get_avg_values
+from regionwise import extract_country_name,extract_star_rating
+from getloveandhateinfo import get_love_and_hate_comment_keywords
+from vote_counter import extract_vote_counts,getvotesinfo
+from average_secondary_metrics import extract_secondary_comments_value,get_avg_values
 
-def write_output(data):
-    with open("insights.json", 'w') as file:
+def write_output(filename , data):
+    with open("./outputs/"+filename, 'w') as file:
         json.dump(data, file, indent=4)
 
 
 if __name__=="__main__":
-    df = pd.read_csv('reviews.csv')
+    df = pd.read_csv('./outputs/reviews.csv')
     output = {}
     
     # votes aggregation
@@ -26,7 +26,7 @@ if __name__=="__main__":
     "downvotes" : downvotes,
     
     }
-    output["votes_aggregation"] = data
+    write_output("votes_aggregation.json" , data)
     
     # regionwise star rating    
 
@@ -37,17 +37,28 @@ if __name__=="__main__":
     # Group by 'country' column and calculate average of 'star_rating'
     avg_ratings = df.groupby('country_name')['star_rating'].mean()
     data = avg_ratings.reset_index().to_dict(orient='records')
-    output["regionwise_rating"] = data
+    
+    write_output("regionwise_rating.json" , data)
     
     # love ane hate keywords
     weighted_keywords = df.apply(lambda row: get_love_and_hate_comment_keywords(row['attributes'], row['id']), axis=1)
     data = weighted_keywords.reset_index().to_dict(orient='records')
-    output["weighted_keywords"] = data
+    
+    augdata={}
+    for ele in data:
+        value = ele[0]
+        if value is None:
+            print("skipped")
+            continue
+        augdata[value["review_id"]] = {"love_keywords" : value["love_keywords"] ,"hate_keywords" : value["hate_keywords"]} 
+    data = augdata
+    write_output("weighted_keywords.json" , data)
     
     # average_secondary_metrics
     df['attributes'].apply(extract_secondary_comments_value)
     data = get_avg_values()
-    output["average_secondary_metrics"] = data
+    
         
-    write_output(output)
+    write_output("average_secondary_metrics.json" , data)
+    
         
