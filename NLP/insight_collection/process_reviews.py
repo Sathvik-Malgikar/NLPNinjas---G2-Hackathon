35f9cad4646eea499473e1817f74d01e127f9e4d
Hemabhushan-r <hemabhushanr3@gmail.com>
2024-04-11 15:14:26 +0530

    Features Extract Funcs Updated

--- NLP/insight_collection/process_reviews.py
+++ NLP/insight_collection/process_reviews.py
@@ -5,6 +5,7 @@ from regionwise import extract_country_name, extract_star_rating
 from getloveandhateinfo import get_love_and_hate_comment_keywords
 from vote_counter import extract_vote_counts, getvotesinfo
 from average_secondary_metrics import extract_secondary_comments_value, get_avg_values
+from tags_extract import extract_features, extract_features_textblob, extract_features_wrapper
 
 
 def write_output(filename, data):
@@ -14,55 +15,66 @@ def write_output(filename, data):
 
 if __name__ == "__main__":
     df = pd.read_csv('./outputs/reviews.csv')
-    output = {}
+    # output = {}
 
-    # votes aggregation
+    # # votes aggregation
 
-    df['attributes'].apply(extract_vote_counts)
-    totalvotes, upvotes, downvotes = getvotesinfo()
+    # df['attributes'].apply(extract_vote_counts)
+    # totalvotes, upvotes, downvotes = getvotesinfo()
 
-    data = {
-        "totalvotes": totalvotes,
-        "upvotes": upvotes,
-        "downvotes": downvotes,
+    # data = {
+    #     "totalvotes": totalvotes,
+    #     "upvotes": upvotes,
+    #     "downvotes": downvotes,
 
-    }
-    write_output("votes_aggregation.json", data)
+    # }
+    # write_output("votes_aggregation.json", data)
 
-    # regionwise star rating
+    # # regionwise star rating
 
-    # Apply the function to extract the rating value
-    df['country_name'] = df['attributes'].apply(extract_country_name)
-    df['star_rating'] = df['attributes'].apply(extract_star_rating)
+    # # Apply the function to extract the rating value
+    # df['country_name'] = df['attributes'].apply(extract_country_name)
+    # df['star_rating'] = df['attributes'].apply(extract_star_rating)
 
-    # Group by 'country' column and calculate average of 'star_rating'
-    avg_ratings = df.groupby('country_name')['star_rating'].mean()
-    data = avg_ratings.reset_index().to_dict(orient='records')
-    aug_data = {}
-    for ele in data:
-        aug_data[ele["country_name"]] = ele["star_rating"]
+    # # Group by 'country' column and calculate average of 'star_rating'
+    # avg_ratings = df.groupby('country_name')['star_rating'].mean()
+    # data = avg_ratings.reset_index().to_dict(orient='records')
+    # aug_data = {}
+    # for ele in data:
+    #     aug_data[ele["country_name"]] = ele["star_rating"]
 
-    data = aug_data
-    write_output("regionwise_rating.json", data)
+    # data = aug_data
+    # write_output("regionwise_rating.json", data)
 
-    # love ane hate keywords
-    weighted_keywords = df.apply(lambda row: get_love_and_hate_comment_keywords(
-        row['attributes'], row['id']), axis=1)
-    data = weighted_keywords.reset_index().to_dict(orient='records')
+    # # love ane hate keywords
+    # weighted_keywords = df.apply(lambda row: get_love_and_hate_comment_keywords(
+    #     row['attributes'], row['id']), axis=1)
+    # data = weighted_keywords.reset_index().to_dict(orient='records')
 
-    augdata = {}
-    for ele in data:
-        value = ele[0]
-        if value is None:
-            print("skipped")
-            continue
-        augdata[value["review_id"]] = {
-            "love_keywords": value["love_keywords"], "hate_keywords": value["hate_keywords"]}
-    data = augdata
-    write_output("weighted_keywords.json", data)
+    # augdata = {}
+    # for ele in data:
+    #     value = ele[0]
+    #     if value is None:
+    #         print("skipped")
+    #         continue
+    #     augdata[value["review_id"]] = {
+    #         "love_keywords": value["love_keywords"], "hate_keywords": value["hate_keywords"]}
+    # data = augdata
+    # write_output("weighted_keywords.json", data)
 
-    # average_secondary_metrics
-    df['attributes'].apply(extract_secondary_comments_value)
-    data = get_avg_values()
+    # # average_secondary_metrics
+    # df['attributes'].apply(extract_secondary_comments_value)
+    # data = get_avg_values()
 
-    write_output("average_secondary_metrics.json", data)
+    # write_output("average_secondary_metrics.json", data)
+
+    extracted_features_from_review = df['attributes'].apply(
+        lambda x: extract_features_wrapper(x, extract_features))
+    extracted_features_textblob_from_review = df['attributes'].apply(
+        lambda x: extract_features_wrapper(x, extract_features_textblob))
+    review_features = {"review_data": extracted_features_from_review.tolist()}
+    review_features_textblob = {
+        "review_data": extracted_features_from_review.tolist()}
+    write_output("extracted_features_spacy.json", review_features)
+    write_output("extracted_features_textblob_polarity.json",
+                 review_features_textblob)
--- NLP/insight_collection/server.py
+++ NLP/insight_collection/server.py
@@ -1,21 +1,23 @@
-from flask import Flask,Response
+from flask import Flask, Response
 import json
 
 from flask_cors import CORS
 
 
 def read_insights(filename):
-    with open("./outputs/" +filename, 'r') as file:
+    with open("./outputs/" + filename, 'r') as file:
         data = json.load(file)
     return data
-        
+
 
 app = Flask(__name__)
 CORS(app)
 
+
 @app.route('/')
 def hello_world():
-	return 'Hello World'
+    return 'Hello World'
+
 
 @app.route('/aggregates/average_secondary_metrics')
 def get_average_secondary_metrics():
@@ -25,6 +27,7 @@ def get_average_secondary_metrics():
     resp.data = json.dumps(json_data)
     return resp
 
+
 @app.route('/aggregates/votes_data')
 def get_votes_data():
     resp = Response()
@@ -32,6 +35,7 @@ def get_votes_data():
     resp.headers["Content-Type"] = "application/json"
     resp.data = json.dumps(json_data)
     return resp
+
 
 @app.route('/aggregates/regionwise_rating')
 def get_regionwise_rating():
@@ -42,9 +46,15 @@ def get_regionwise_rating():
     return resp
 
 
+@app.route('/rag/query')
+def query_rag_gemma():
+    resp = Response()
+    return resp
+
+
 # main driver function
 if __name__ == '__main__':
-    
-	# run() method of Flask class runs the application 
-	# on the local development server.
+
+    # run() method of Flask class runs the application
+    # on the local development server.
     app.run()
