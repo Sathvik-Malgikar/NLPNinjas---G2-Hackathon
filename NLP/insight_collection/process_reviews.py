eba96a566298935c0d2763662d9e7efd5d5f24bb
Sathvik-Malgikar <Sathvik.malgikar@gmail.com>
2024-04-12 07:54:48 +0530

    merging keywords set done

--- NLP/insight_collection/process_reviews.py
+++ NLP/insight_collection/process_reviews.py
@@ -12,8 +12,63 @@ def write_output(filename, data):
     with open("./outputs/"+filename, 'w') as file:
         json.dump(data, file, indent=4)
 
+def read_json_from_file(file_path):
+    with open(file_path, 'r') as file:
+        data = json.load(file)
+    return data
+
+def read_keywords(column_value,review_id):
+    try:
+        
+        data = eval(column_value)
+        love_keywords = data["love_keywords"]
+        hate_keywords = data["hate_keywords"]
+        
+        return ({"review_id" : review_id, "love_keywords" : love_keywords,"hate_keywords" : hate_keywords})
+    except Exception as e:
+        print(e)
+        print("keyword extraction failed!")
+
+def merge_keywords_set():
+    keywords_set_1 = read_json_from_file("./outputs/weighted_keywords.json")
+    keywords_set_2 = pd.read_csv('./outputs/reviews_bert_keywords.csv')
+    
+    keywords_set_2 = keywords_set_2.apply(lambda row: read_keywords(row['attributes'], row['id']), axis=1)
+    keywords_set_2 = keywords_set_2.reset_index().to_dict(orient='records')
+    
+    augdata = {}
+    for ele in keywords_set_2:
+        # print(ele[0])
+        augdata[ele[0]["review_id"]] = {"love_keywords": ele[0]["love_keywords"], "hate_keywords": ele[0]["hate_keywords"]}
+    
+    keywords_set_2 = augdata
+    write_output("weighted_keywords_bert.json",keywords_set_2)
+    
+    combined_set = {}
+    # MERGE SETS NOW
+    # print ("7098976" in keywords_set_1)
+    # print (7098976 in keywords_set_2)
+    review_ids =  set.union(set(keywords_set_1.keys()),set(map(str,keywords_set_2.keys()))) 
+    for r_id in review_ids:
+        temp_l_words=[]
+        temp_h_words=[]
+        if r_id in keywords_set_1:
+            temp_l_words.extend(keywords_set_1[r_id]["love_keywords"])
+            temp_h_words.extend(keywords_set_1[r_id]["hate_keywords"])
+            
+        if int(r_id) in keywords_set_2:
+            temp_l_words.extend(keywords_set_2[int(r_id)]["love_keywords"])
+            temp_h_words.extend(keywords_set_2[int(r_id)]["hate_keywords"])
+        
+        combined_set[r_id] = {"love_keywords": temp_l_words, "hate_keywords": temp_h_words}
+    
+    write_output("combined_keywords.json",combined_set)
+    
+    
 
 if __name__ == "__main__":
+    merge_keywords_set()
+    exit()
     df = pd.read_csv('./outputs/reviews.csv')
     # output = {}
 
