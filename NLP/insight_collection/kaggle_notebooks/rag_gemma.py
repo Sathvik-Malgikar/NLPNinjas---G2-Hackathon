13fdbb2dbbad24c73613761b044aa753e5ae8d2a
Pranav Ambiga <pranav.ambig123@gmail.com>
2024-04-13 01:21:06 +0530

    minor fixes

--- NLP/insight_collection/kaggle_notebooks/rag_gemma.py
+++ NLP/insight_collection/kaggle_notebooks/rag_gemma.py
@@ -349,7 +349,7 @@ chain = (
 
 # In[30]:
 
-query='Hello, how are you?'
+query='what is the general sentiment of reviews on G2'
 query_res=chain.invoke(query).replace('\n','')
 print(f"StartResults {query_res} EndResults")
 
